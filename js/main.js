ae8c23a7f0e9e180c4487e9972737cfaa5c4d34f
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2022-12-13 17:45:45 +0500

    Add links

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let elDocRow = document.querySelector(".row");
 
-for (i = 0; i < pokemons.length; i++) {
+for (var list of pokemons) {
   let pokCard = document.createElement("div");
   pokCard.setAttribute(
     "class",
@@ -9,32 +9,32 @@ for (i = 0; i < pokemons.length; i++) {
 
   let idBox = document.createElement("h4");
   idBox.setAttribute("class", "card-title");
-  idBox.textContent = pokemons.id;
+  idBox.textContent = list.id;
   //   Pokemon ID
 
   let nameBox = document.createElement("h3");
   nameBox.setAttribute("class", "card-title");
-  nameBox.textContent = pokemons.name;
+  nameBox.textContent = list.name;
   //   Pokemon name
 
   let imgBox = document.createElement("img");
-  imgBox.setAttribute("src", pokemons.img);
+  imgBox.setAttribute("src", list.img);
   //   Pokemon img
 
   let heightBox = document.createElement("p");
   heightBox.setAttribute("class", "card-text");
-  heightBox.textContent = `Height: ${pokemons.height}`;
+  heightBox.textContent = `Height: ${list.height}`;
   //   Pokemon height
 
   let WeightBox = document.createElement("p");
   WeightBox.setAttribute("class", "card-text");
-  WeightBox.textContent = `Weight: ${pokemons.weight}`;
-  //   Pokemon height
+  WeightBox.textContent = `Weight: ${list.weight}`;
+  //   Pokemon weight
 
   let spawnBox = document.createElement("p");
   spawnBox.setAttribute("class", "card-text");
-  spawnBox.textContent = `Birth time: ${pokemons.spawn_time}`;
-  //   Pokemon height
+  spawnBox.textContent = `Birth time: ${list.spawn_time}`;
+  //   Pokemon birthtime
 
   let cardBody = document.createElement("div");
   cardBody.setAttribute("class", "card-body");
